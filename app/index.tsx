7bb2cccb036be0c24444b5352b08ba3b289d7320
Vaishnav <vaishnavchandurkar12@gmail.com>
2024-07-14 12:17:08 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { SafeAreaView, Text } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: "center",
+          fontWeight: 500,
+        }}
+      >
+        Editor
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
